22f5130db7dedcedb22226167d70f26b83eacb86
abbytheastronomer <ayc35@sfu.ca>
2021-03-18 20:12:09 -0700

    added javascript

---
+++ IAT 339 styleguide P02/scripts/main.js
@@ -0,0 +1,14 @@
+"use strict"
+
+
+
+function mySubscribed() {
+	alert("You have subscribed");
+}
+
+function myRegistered() {
+	alert("You have Registered");
+}
+
+//<!-- https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_confirm -->
+// week 7 lab and lecture
